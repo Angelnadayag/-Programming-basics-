e50bd2f0882b8089fdc053e6ab2a705128a90062
Angelnadayag <angelnadayag.202300527@gmail.com>
2025-02-01 10:35:46 +0800

    new

---
+++ script.js
@@ -0,0 +1,25 @@
+let output = "";
+// Variables
+let name = "Angel Nadayag";
+let age = 19;
+let isStudent = true;
+output += `Name: ${name}, Age: ${age}, Student: ${isStudent}\n`;
+
+// Loops
+for (let i = 1; i <= 10; i++) {
+    output += i + "\n";
+}
+
+// Functions
+function addNumbers(a, b) {
+    return a + b;
+}
+output += addNumbers(5, 10) + "\n";
+
+// Conditionals
+if (age >= 18) {
+    output += "Adult\n";
+} else {
+    output += "Minor\n";
+}
+document.getElementById('console-output').innerText = output;
